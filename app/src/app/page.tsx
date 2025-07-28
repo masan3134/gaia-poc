03cc7530e720180406919fe0916c2bdb6ef8d76b
masan3134 <masan3134@github.com>
2025-07-28 21:22:58 +0300

    Fix GaiaListener import path and placement

--- app/src/app/page.tsx
+++ app/src/app/page.tsx
@@ -1,11 +1,10 @@
+import GaiaListener from './components/GaiaListener';
 'use client'
-import GaiaListener from '../../components/GaiaListener'
 
 export default function Home() {
   return (
     <main style={{ padding: 20 }}>
       <h1>GAIA Panel</h1>
-      <GaiaListener />
     </main>
   )
 }
